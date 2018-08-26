eb5363efaf645094e6fb5bd5f3aba61ae3da608d
Ben Echols <benjamin.echols@gmail.com>
2018-08-25 21:25:04 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <p>Giving out free Mom (and Dad, little sister, big brother, auntie, uncle) hugs at Boulder Pridefest. We are a group of Boulder County residents representing many backgrounds and various faith communities who fully support LGBTQIA folx and are here to offer hugs and support to those who need it. </p>
+    <a href="https://www.facebook.com/events/273278053502860/">Join us on Facebook</a>
+  </Layout>
+)
+
+export default IndexPage
